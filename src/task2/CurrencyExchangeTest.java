c19dbed9b92f0361e5bd93a14450f344129a0dd1
Devloner <tminecraftben@gmail.com>
2023-03-17 16:08:45 +0200

    USD to UAN implemented and tested

--- src/task2/CurrencyExchangeTest.java
+++ src/task2/CurrencyExchangeTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.assertEquals;
 
 public class CurrencyExchangeTest {
     @Test
-    public void UAHtoUSD_test() {
+    public void UAH_to_USD_test() {
         CurrencyExchanger ex = new CurrencyExchanger();
-        double amount = ex.convertTo(1, CurrencyExchanger.Currencies.UAH);
-        assertEquals(amount, 0.027, 0.0001);
+        double amount = ex.convertTo(1, CurrencyExchanger.Currencies.UAH, CurrencyExchanger.Currencies.USD);
+        assertEquals(0.027, amount, 0.001);
+    }
+
+    @Test
+    public void USD_to_UAH_test() {
+        CurrencyExchanger ex = new CurrencyExchanger();
+        double amount = ex.convertTo(1, CurrencyExchanger.Currencies.USD, CurrencyExchanger.Currencies.UAH);
+        assertEquals(36.93, amount, 0.001);
     }
 }
--- src/task2/CurrencyExchanger.java
+++ src/task2/CurrencyExchanger.java
@@ -2,17 +2,30 @@ package task2;
 
 public class CurrencyExchanger {
 
-    public double convertTo(double originAmount, Currencies originCurrency) {
-        return originAmount * originCurrency.ratioToMainCurrency;
+    public CurrencyExchanger() {
+        Currencies.mainCurrency = Currencies.USD;
+    }
+
+    public double convertTo(double originAmount, Currencies originCurrency, Currencies targetCurrency) {
+        if(originCurrency != Currencies.mainCurrency)
+            return originAmount * originCurrency.ratioToMainCurrency;
+        else
+            return originAmount * targetCurrency.ratioFromMainCurrency;
     }
 
     public enum Currencies {
-        UAH(0.027);
+        USD(1, 1),
+        PLN(0.23, 4.41),
+        UAH(0.027, 36.93);
 
-        private final double ratioToMainCurrency;
+        private static Currencies mainCurrency;
 
-        Currencies(double ratioToMainCurrency) {
+        private double ratioToMainCurrency;
+        private double ratioFromMainCurrency;
+
+        Currencies(double ratioToMainCurrency, double ratioFromMainCurrency) {
             this.ratioToMainCurrency = ratioToMainCurrency;
+            this.ratioFromMainCurrency = ratioFromMainCurrency;
         }
     }
 }
