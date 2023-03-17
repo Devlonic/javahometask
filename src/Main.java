5cf7bc697a362fd01560bd2aa841b04a8f88bf2b
Devloner <tminecraftben@gmail.com>
2023-03-17 12:06:52 +0200

    task2 begin

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,50 @@
 import java.time.LocalDate;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        // task1
+
+        //1
         Predicate isLeapYear = (year)-> (int)year % 4 == 0;
 
-        Duration d = Duration.of(10, ChronoUnit.HOURS);
-        System.out.println(isLeapYear.test(2004));
-        System.out.println(isLeapYear.test(2005));
-        System.out.println(LocalDate.of(2023, 3, 13).);
-
-        BiFunction<LocalDate, LocalDate, Integer> getDaysCount = (LocalDate fromDate, LocalDate toDate)-> {
-            return 0;
+        //2
+        BiFunction<Date, Date, Integer> countDaysBetweenDates = (d1, d2)->{
+            var millisDiff = d1.getTime() - d2.getTime();
+            // compiler should optimize this code
+            return (int)((double)millisDiff / (24*60*60*1000)-1);
         };
+        System.out.println(countDaysBetweenDates.apply(
+                new Date(2023,3,18),
+                new Date(1000,3,7)));
+
+        //3
+
+        BiFunction<Date, Date, Integer> countFullWeeksBetweenDates = (d1, d2)->{
+            var millisDiff = d1.getTime() - d2.getTime();
+            // compiler should optimize this code
+            return (int)((double)millisDiff / (7*24*60*60*1000)-1);
+        };
+        System.out.println(countFullWeeksBetweenDates.apply(
+                new Date(2023,3,18),
+                new Date(2022,9,7)));
+
+        //4
+        // todo
+        Function<Date, Integer> dayOfWeek = (d)->{
+            return -1;
+        };
+        System.out.println(dayOfWeek.apply(new Date(2022,9,7)));
+
+        //task2
+        //1
+
     }
 }
