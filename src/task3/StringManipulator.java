29290aa9d5a3c4ea0ceb77571b3566f77178600f
Devloner <tminecraftben@gmail.com>
2023-03-25 13:26:19 +0200

    task3 done and tested, branch done

--- src/task3/StringManipulator.java
+++ src/task3/StringManipulator.java
@@ -4,16 +4,71 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringManipulator {
     public static boolean isStringPalindrome(String str) {
-        //todo
-        return true;
+        if(str == null)
+            return false;
+
+        String buf = str.toLowerCase();
+
+        var orig = buf.chars().mapToObj(c-> (char)c).filter(c->Character.isLetterOrDigit(c)).collect(Collectors.toList());
+
+        var reversed = buf.chars().mapToObj(c-> (char)c).filter(c->Character.isLetterOrDigit(c)).collect(Collectors.toList());
+
+        Collections.reverse(reversed);
+
+        return orig.equals(reversed);
     }
 
-    public static int getCountVowelsLetters(String str) {
-        //todo
-        return 0;
+    private static final List<Character> vowels = List.of(
+            'а', 'е', 'є', 'и', 'і', 'ї', 'о', 'у', 'ю', 'я', 'a', 'e', 'i', 'o', 'u', 'y'
+    );
+    public static long getCountVowelsLetters(String str) {
+        Predicate isVowel = (s) -> {
+          return vowels.contains(Character.toLowerCase((char)s));
+        };
+
+        var x = str.
+                toLowerCase().
+                chars().
+                mapToObj(c-> (char)c).
+                filter(isVowel).
+                count();
+        return x;
+    }
+
+
+    private static final List<Character> consonants = List.of(
+            'b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 's', 't', 'v', 'x', 'z', 'h', 'r', 'w',
+            'б', 'в', 'г', 'ґ', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш'
+    );
+    public static long getCountConsonantLetters(String str) {
+        Predicate isConsonant = (s) -> {
+            return consonants.contains(Character.toLowerCase((char)s));
+        };
+
+        var x = str.
+                toLowerCase().
+                chars().
+                mapToObj(c-> (char)c).
+                filter(isConsonant).
+                count();
+        return x;
+    }
+
+    public static int getWordCountInString(String str, String word) {
+        Pattern pattern = Pattern.compile(word, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
     }
 }
--- src/task3/StringManipulatorTest.java
+++ src/task3/StringManipulatorTest.java
@@ -2,13 +2,33 @@ package task3;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class StringManipulatorTest {
     @Test
     public void StringPalindromeTest() {
-        var t = StringManipulator.isStringPalindrome("Уму - мінімуму!");
+        var t = StringManipulator.isStringPalindrome("Уму - мінімуму");
         assertTrue(t);
+
+        var f = StringManipulator.isStringPalindrome("Уму - мінімуму111");
+        assertFalse(f);
+    }
+
+    @Test
+    public void getCountVowelsLetters_Test() {
+        var c = StringManipulator.getCountVowelsLetters("FFаеєиіїоуюяaeiouyVV003355++==");
+        assertEquals(16, c);
+    }
+
+    @Test
+    public void getCountConsonantsLetters_Test() {
+        var c = StringManipulator.getCountConsonantLetters("bcdfgjklmnpqstvxzhrwбвгґджзйклмннпрстфхцчш");
+        assertEquals(42, c);
+    }
+
+    @Test
+    public void getWordCountInString_Test() {
+        var c = StringManipulator.getWordCountInString("Lorem Ipsum is simply dummy Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when anm lorem unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem", "lorem");
+        assertEquals(5, c);
     }
 }
