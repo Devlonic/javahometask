a358d1cc2ff48aa8c6f2481e52c81220aa01090a
Devloner <tminecraftben@gmail.com>
2023-03-25 11:56:23 +0200

    task4 done and tested

--- src/task4/MeasuresConverter.java
+++ src/task4/MeasuresConverter.java
@@ -1,21 +1,30 @@
 package task4;
 
+
 public class MeasuresConverter {
     public double convertTo(double originAmount, Measures originMeasure, Measures targetMeasure) {
-
+        // non-meters -> non-meters
+        if(originMeasure != Measures.METER && targetMeasure !=  Measures.METER) {
+            var originToMeterAmount = convertTo(originAmount, originMeasure, Measures.METER);
+            return convertTo(originToMeterAmount, Measures.METER, targetMeasure);
+        } else if(originMeasure == Measures.METER) // meters to any
+            // result is originAmount * (targetMeasure.ratioToMeter^(-1))
+            return originAmount * Math.pow(targetMeasure.ratioToMeter, -1);
+        else
+            return originAmount * originMeasure.ratioToMeter;
     }
 
     public enum Measures {
-        MILLIMETER(0.000001),
+        MILLIMETER(0.001),
         SANTIMETER(0.01),
         DECIMETER(0.1),
         METER(1),
         KILOMETER(1000);
 
-        private double ratioToMeter;
+        private final double ratioToMeter;
 
-        Measures(double ratioToMainMeasure) {
-            this.ratioToMeter = ratioToMainMeasure;
+        Measures(double ratioToMeter) {
+            this.ratioToMeter = ratioToMeter;
         }
     }
 }
--- src/task4/MeasuresConverterTest.java
+++ src/task4/MeasuresConverterTest.java
@@ -11,4 +11,32 @@ public class MeasuresConverterTest {
         double amount = ex.convertTo(1, MeasuresConverter.Measures.METER, MeasuresConverter.Measures.SANTIMETER);
         assertEquals(100, amount, 0.0001);
     }
+
+    @Test
+    public void SANTIMETERS_TO_METERS_test() {
+        MeasuresConverter ex = new MeasuresConverter();
+        double amount = ex.convertTo(100, MeasuresConverter.Measures.SANTIMETER, MeasuresConverter.Measures.METER);
+        assertEquals(1, amount, 0.0001);
+    }
+
+    @Test
+    public void METERS_TO_KILOMETERS_test() {
+        MeasuresConverter ex = new MeasuresConverter();
+        double amount = ex.convertTo(1, MeasuresConverter.Measures.METER, MeasuresConverter.Measures.KILOMETER);
+        assertEquals(0.001, amount, 0.0001);
+    }
+
+    @Test
+    public void DECIMETERS_TO_MILLIMETERS_test() {
+        MeasuresConverter ex = new MeasuresConverter();
+        double amount = ex.convertTo(1, MeasuresConverter.Measures.DECIMETER, MeasuresConverter.Measures.MILLIMETER);
+        assertEquals(100, amount, 0.0001);
+    }
+
+    @Test
+    public void MILLIMETERS_TO_KILOMETERS_test() {
+        MeasuresConverter ex = new MeasuresConverter();
+        double amount = ex.convertTo(1000000, MeasuresConverter.Measures.MILLIMETER, MeasuresConverter.Measures.KILOMETER);
+        assertEquals(1, amount, 0.0001);
+    }
 }
