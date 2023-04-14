71b42e61a277e1a1d573b6df3bc19a764209037b
Devloner <tminecraftben@gmail.com>
2023-04-14 12:54:57 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,28 @@ class Task1 {
     }
 }
 
+class Task2 {
+    public static void start() {
+        String file1path = "./dir/file1.txt";
+
+        try {
+            Scanner s1 = new Scanner(new File(file1path));
+            String maxString = "";
+            while (s1.hasNextLine()) {
+                var row1 = s1.nextLine();
+                if(row1.length() > maxString.length())
+                    maxString = row1;
+            }
+            System.out.println(maxString.length() + " " + maxString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Task1.start();
+//        Task1.start();
+        Task2.start();
     }
 }
