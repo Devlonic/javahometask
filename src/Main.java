818eb4a9957859803d76c8048cbb0295be56b420
Devloner <tminecraftben@gmail.com>
2023-04-14 13:23:31 +0300

    task3 done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Task1 {
@@ -45,9 +46,45 @@ class Task2 {
     }
 }
 
+class Task3 {
+    public static void start() {
+        String file1path = "./dir/file3.txt";
+
+        try {
+            Scanner s1 = new Scanner(new File(file1path));
+            System.out.printf("                \t\tMIN\tMAX\tSUM\n");
+            List<Integer> minimums = new ArrayList<>();
+            List<Integer> maximums = new ArrayList<>();
+            int totalSum = 0;
+            while (s1.hasNextLine()) {
+                var row1 = s1.nextLine();
+                var rawNumbers = Arrays.stream(row1.split(" ")).map(s-> Integer.valueOf(s)).collect(Collectors.toList());
+                var max = rawNumbers.stream().mapToInt(Integer::intValue).max().getAsInt();
+                var min = rawNumbers.stream().mapToInt(Integer::intValue).min().getAsInt();
+                var sum = rawNumbers.stream().mapToInt(Integer::intValue).sum();
+                minimums.add(min);
+                maximums.add(max);
+                totalSum+=sum;
+                System.out.printf("%s:\t\t%d\t%d\t%d\n",
+                        Arrays.toString(rawNumbers.toArray()),
+                        min,
+                        max,
+                        sum);
+            }
+            System.out.printf("%s:\t\t%d\t%d\t%d\n", "TOTAL          ",
+                    minimums.stream().mapToInt(Integer::intValue).min().getAsInt(),
+                    maximums.stream().mapToInt(Integer::intValue).max().getAsInt(),
+                    totalSum);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
 //        Task1.start();
-        Task2.start();
+//        Task2.start();
+        Task3.start();
     }
 }
